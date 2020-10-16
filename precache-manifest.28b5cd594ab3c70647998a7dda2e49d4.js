9544da6599ae24ecfb33eb6b6bacc62b26e3c8f5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-16 09:50:18 +0000

    Deploy collinewait/ika-gmail-scraper-frontend to github.com/collinewait/ika-gmail-scraper-frontend.git:gh-pages

---
+++ precache-manifest.28b5cd594ab3c70647998a7dda2e49d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6316368c26a308c134b4302cfbfbaa7e",
+    "url": "/ika-gmail-scraper-frontend/index.html"
+  },
+  {
+    "revision": "6a9cb9ed489b4579f3cf",
+    "url": "/ika-gmail-scraper-frontend/static/css/main.e8611cb9.chunk.css"
+  },
+  {
+    "revision": "2c97011cafcde3325732",
+    "url": "/ika-gmail-scraper-frontend/static/js/2.b132448e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ika-gmail-scraper-frontend/static/js/2.b132448e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a9cb9ed489b4579f3cf",
+    "url": "/ika-gmail-scraper-frontend/static/js/main.f0974238.chunk.js"
+  },
+  {
+    "revision": "c567dae9433abe3d8740",
+    "url": "/ika-gmail-scraper-frontend/static/js/runtime-main.bfb4fd1a.js"
+  }
+]);
